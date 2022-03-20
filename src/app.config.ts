1463e12ee968703e07899b48054efe3ed144ff8d
song <3224266014@qq.com>
2022-03-20 22:02:30 +0800

    feat: init

---
+++ src/app.config.ts
@@ -0,0 +1,17 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  },
+  // subpackages: [ // 分包
+  //   {
+  //     // root: 'pages/featureA',
+  //     // pages: ['index/index']
+  //   }
+  // ]
+})
