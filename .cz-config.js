82131acfcfc0d98891398178025a10437617c1fa
song <3224266014@qq.com>
2022-03-21 10:26:58 +0800

    feat: cz

--- .cz-config.js
+++ .cz-config.js
@@ -64,8 +64,7 @@ module.exports = {
     // 选择 scope: custom 时会出下面的提示
     customScope: '请输入自定义的 scope:',
     subject: '填写一个简短精炼的描述语句:\n',
-    body:
-      '添加一个更加详细的描述，可以附上新增功能的描述或 bug 链接、截图链接 (可选)。使用 "|" 换行:\n',
+    body: '添加一个更加详细的描述，可以附上新增功能的描述或 bug 链接、截图链接 (可选)。使用 "|" 换行:\n',
     breaking: '列举非兼容性重大的变更 (可选):\n',
     footer: '列举出所有变更的 ISSUES CLOSED  (可选)。 例如.: #31, #34:\n',
     confirmCommit: '确认提交?'
